36b9133e32b4fcf5e1d341a2d5684155511225f6
프프 <143497019+K-angly@users.noreply.github.com>
2023-11-29 15:10:10 +0900

    Delete src directory

--- src/main/frontend/src/Components/LogoutPage.js
+++
@@ -1,8 +0,0 @@
-import {KAKAO_AUTH_URL, KAKAO_LOGOUT_URL} from "./OAuth";
-import React from "react";
-
-export default () => (
-    <a href={KAKAO_LOGOUT_URL}>
-        로그아웃
-    </a>
-)
--- src/main/java/com/team2/howeather/Domain/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team2.howeather.Domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
